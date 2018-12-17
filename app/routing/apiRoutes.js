5d533a897e1b407efd5fe709becfa7946a04ec86
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-12-17 10:09:33 -0500

    updated apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,41 +1,48 @@
-var friends = require("../app/data/friends");
+// Pull in required dependencies
+var path = require("path");
 
+// Import the list of friend entries
+var friends = require("../data/friends.js");
+
+// Export API routes
 module.exports = function(app) {
+  // Total list of friend entries
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
+  // Add new friend entry
   app.post("/api/friends", function(req, res) {
-    console.log(req.body);
-    var user = req.body;
-    var userScores = user.scores;
+    // Capture the user input object
+    var userInput = req.body;
 
-    var bestMatch = friends[0];
-    var bestScore = 100;
-    var compScore = 0;
+    var userResponses = userInput.scores;
 
-    for (var i in friends) {
-      for (var j = 0; j < 10; j++) {
-        compScore += Math.abs(
-          parseInt(friends[i].scores[j]) - parseInt(userScores[j])
-        );
-        console.log(compScore);
-      }
-      if (bestScore > compScore && user.name != friends[i].name) {
-        bestMatch = friends[i];
-        bestScore = compScore;
-      }
-      compScore = 0;
-    }
-    var nameFlag = true;
+    // Compute best friend match
+    var matchName = "";
+    var matchImage = "";
+    var totalDifference = 10000; // Make the initial value big for comparison
+
+    // Examine all existing friends in the list
     for (var i = 0; i < friends.length; i++) {
-      if (user.name == friends[i].name) {
-        nameFlag = false;
+      // Compute differenes for each question
+      var diff = 0;
+      for (var j = 0; j < userResponses.length; j++) {
+        diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+      }
+
+      // If lowest difference, record the friend match
+      if (diff < totalDifference) {
+        totalDifference = diff;
+        matchName = friends[i].name;
+        matchImage = friends[i].photo;
       }
     }
-    if (nameFlag) {
-      friends.push(req.body);
-    }
-    res.json(bestMatch);
+
+    // Add new user
+    friends.push(userInput);
+
+    // Send appropriate response
+    res.json({ status: "OK", matchName: matchName, matchImage: matchImage });
   });
 };
