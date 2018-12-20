0007a1b24f4afe44bcd9a6db14d745316dfa5668
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-12-20 09:20:11 -0500

    update to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,61 +1,63 @@
 // Data structure that will store the friends data
 var friendsArray = [
   {
-    name: "Scooter",
+    name: "Leslie Knope",
     photo:
-      "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Leslie_Knope_%28played_by_Amy_Poehler%29.png/240px-Leslie_Knope_%28played_by_Amy_Poehler%29.png",
     scores: ["1", "5", "5", "1", "5", "5", "1", "1", "1", "5"]
   },
   {
-    name: "Fluffy",
-    photo: "https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg",
+    name: "Ron Swanson",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/RonSwanson.jpg/250px-RonSwanson.jpg",
     scores: ["2", "4", "1", "1", "1", "1", "1", "2", "1", "5"]
   },
   {
-    name: "Spencer",
+    name: "Andy Dwyer",
     photo:
-      "http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Andy_Dwyer.jpg/175px-Andy_Dwyer.jpg",
     scores: ["4", "3", "5", "1", "1", "5", "5", "1", "1", "1"]
   },
   {
-    name: "Zackariah",
+    name: "Tom Haverford",
     photo:
-      "https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/1/1f/Tom_Haverford.jpg/260px-Tom_Haverford.jpg",
     scores: ["5", "1", "2", "1", "3", "4", "1", "3", "3", "4"]
   },
   {
-    name: "Cloe",
+    name: "Ann Perkins",
     photo:
-      "http://4.bp.blogspot.com/-HTvSYzA-pO4/UgQb4Zh_u0I/AAAAAAAAEuI/XwhtogT_1tA/s1600/3+cute2.jpg",
+      "https://vignette.wikia.nocookie.net/parksandrecreation/images/3/33/Ann_Perkins.jpg/revision/latest?cb=20180116070354",
     scores: ["1", "1", "5", "5", "5", "5", "5", "5", "5", "1"]
   },
   {
-    name: "Mrs. Piggy",
+    name: "Chris Trager",
     photo:
-      "http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Chris_Traeger.jpg/218px-Chris_Traeger.jpg",
     scores: ["1", "1", "5", "1", "3", "2", "5", "1", "1", "5"]
   },
   {
-    name: "Beyonce Knowles",
+    name: "Ben Wyatt",
     photo:
-      "http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/0/02/Ben_Wyatt_Parks_%26_Rec.jpg/235px-Ben_Wyatt_Parks_%26_Rec.jpg",
     scores: ["5", "4", "4", "2", "4", "4", "3", "2", "3", "3"]
   },
   {
-    name: "Adele Adkins",
-    photo: "https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg",
+    name: "April Ludgate",
+    photo:
+      "https://vignette.wikia.nocookie.net/parksandrecreation/images/e/ed/April-0.png/revision/latest?cb=20180116070637",
     scores: ["5", "2", "3", "1", "5", "1", "3", "1", "1", "5"]
   },
   {
-    name: "Kermit the Frog",
+    name: "Jerry Gergich",
     photo:
-      "http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/d/d1/Jerry_Gergich.jpg/220px-Jerry_Gergich.jpg",
     scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
   },
   {
-    name: "Elmo",
+    name: "Donna Meagle",
     photo:
-      "http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/0/0a/Donna_Meagle.jpg/250px-Donna_Meagle.jpg",
     scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
   }
 ];
